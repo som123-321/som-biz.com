c75af16b5f19499093d22d13740ab7cdc7fb1199
som123-321 <info@som-biz.com>
2025-12-23 15:41:59 +0900

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,34 +1,22 @@
-{\rtf1\ansi\ansicpg932\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red0\green0\blue255;\red255\green255\blue254;
-\red14\green110\blue109;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;\cssrgb\c0\c0\c100000;\cssrgb\c100000\c100000\c99608;
-\cssrgb\c0\c50196\c50196;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
 
-\f0\fs24 \cf0 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 \
-\cf3 \cb4 \strokec3 export\cf0 \strokec2  \cf3 \strokec3 interface\cf0 \strokec2  \cf5 \strokec5 ServiceFeature\cf0 \strokec2  \{\cb1 \
-\cb4   label: \cf3 \strokec3 string\cf0 \strokec2 ;\cb1 \
-\cb4   iconName: \cf3 \strokec3 string\cf0 \strokec2 ;\cb1 \
-\cb4   description: \cf3 \strokec3 string\cf0 \strokec2 ;\cb1 \
-\cb4 \}\cb1 \
-\
-\cf3 \cb4 \strokec3 export\cf0 \strokec2  \cf3 \strokec3 interface\cf0 \strokec2  \cf5 \strokec5 ServiceInfo\cf0 \strokec2  \{\cb1 \
-\cb4   id: \cf3 \strokec3 string\cf0 \strokec2 ;\cb1 \
-\cb4   title: \cf3 \strokec3 string\cf0 \strokec2 ;\cb1 \
-\cb4   description: \cf3 \strokec3 string\cf0 \strokec2 ;\cb1 \
-\cb4   imageUrl: \cf3 \strokec3 string\cf0 \strokec2 ;\cb1 \
-\cb4   features: \cf5 \strokec5 ServiceFeature\cf0 \strokec2 [];\cb1 \
-\cb4 \}\cb1 \
-\
-\cf3 \cb4 \strokec3 export\cf0 \strokec2  \cf3 \strokec3 interface\cf0 \strokec2  \cf5 \strokec5 CompanyInfo\cf0 \strokec2  \{\cb1 \
-\cb4   name: \cf3 \strokec3 string\cf0 \strokec2 ;\cb1 \
-\cb4   representative: \cf3 \strokec3 string\cf0 \strokec2 ;\cb1 \
-\cb4   address: \cf3 \strokec3 string\cf0 \strokec2 ;\cb1 \
-\cb4   established: \cf3 \strokec3 string\cf0 \strokec2 ;\cb1 \
-\cb4   businessDetails: \cf3 \strokec3 string\cf0 \strokec2 [];\cb1 \
-\cb4 \}\cb1 \
+export interface ServiceFeature {
+  label: string;
+  iconName: string;
+  description: string;
+}
+
+export interface ServiceInfo {
+  id: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+  features: ServiceFeature[];
+}
+
+export interface CompanyInfo {
+  name: string;
+  representative: string;
+  address: string;
+  established: string;
+  businessDetails: string[];
 }
